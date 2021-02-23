e30b0a0c5b5d5d3ac5e29ce2a4dd9fab1d5af723
Ghosh <ghoss8@l100400min-1.local.rpega.com>
2021-02-23 10:07:11 +0530

    updated

--- work-tmux.sh
+++ work-tmux.sh
@@ -2,7 +2,7 @@
 
 editor='nvim'
 
-tmux new-session -d -s "dev" -n "p-notes" -c ~/workspaces/personal/notes $editor
-tmux new-window -c ~/Box/Zim_Notes/ -n "notes" $editor
-tmux new-window -c ~/workspaces/git_workspaces/SDN -n "vna"
+tmux new-session -d -s "dev" -n "notes" -c ~/workspaces/personal/notes $editor
+tmux new-window -c ~/"OneDrive - Pegasystems Inc"/notes/ -n "pega-notes" $editor
+tmux new-window -c ~/workspaces/pega/prpc-platform -n "prpc-platform"
 tmux attach-session -d -c "~"
